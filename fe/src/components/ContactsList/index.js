35d0cc3ca5a2a39d907c06423a6153963dd53295
Leandro Filie <leandro.gfilie@gmail.com>
2022-07-14 19:13:49 -0300

    feat(ContactsList): create card

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -1,4 +1,11 @@
-import { Container, Header } from './styles';
+import {
+  Card,
+  Container, Header, ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +14,79 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo Contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+      </ListContainer>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Leandro Filié</strong>
+            <small>instagram</small>
+          </div>
+          <span>leandro@email.com</span>
+          <span>(16) 99999-99999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Leandro Filié</strong>
+            <small>instagram</small>
+          </div>
+          <span>leandro@email.com</span>
+          <span>(16) 99999-99999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Leandro Filié</strong>
+            <small>instagram</small>
+          </div>
+          <span>leandro@email.com</span>
+          <span>(16) 99999-99999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+
+          <button type="button">
+            <img src={trash} alt="Delete" />
+          </button>
+        </div>
+      </Card>
+
     </Container>
   );
 }
--- fe/src/components/ContactsList/styles.js
+++ fe/src/components/ContactsList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   justify-content: space-between;
 
   strong {
-    color: #222;
     font-size: 24px;
   }
 
@@ -18,7 +17,7 @@ export const Header = styled.header`
     color: ${({ theme }) => theme.colors.primary.main};
     text-decoration: none;
     font-weight: bold;
-    border: 2px solid ${({ theme }) => theme.colors.primary.main};;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
     border-radius: 4px;
     padding: 8px 16px;
     transition: all .2s ease-in;
@@ -29,4 +28,77 @@ export const Header = styled.header`
       transition: all .2s ease-in;
     }
 }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+    button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
+
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+  }
+  }
+`;
+
+export const Card = styled.div`
+  background: #FFF;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  padding: 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+      margin-bottom: 4px;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        text-transform: uppercase;
+        margin-left: 4px;
+        padding: 4px;
+        border-radius: 4px;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+    a {
+
+    }
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
+    }
+  }
 `;
