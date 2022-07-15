682579c409549574a0907fb93dd9a6704bfbd7af
Leandro Filie <leandro.gfilie@gmail.com>
2022-07-15 16:57:28 -0300

    feat(Modal): add danger status in title and button

--- fe/src/components/Modal/index.js
+++ fe/src/components/Modal/index.js
@@ -2,16 +2,16 @@ import { Container, Overlay, Footer } from './styles';
 
 import Button from '../Button';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Título do Modal</h1>
         <p>Corpo do Modal</p>
 
         <Footer>
           <button type="button" className="cancel-button">Cancelar</button>
-          <Button type="button">Deletar</Button>
+          <Button type="button" danger={danger}>Deletar</Button>
         </Footer>
       </Container>
     </Overlay>
