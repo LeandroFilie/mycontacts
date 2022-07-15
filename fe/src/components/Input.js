896d7d0d610dc1b427628577d9870a2dc2c74425
Leandro Filie <leandro.gfilie@gmail.com>
2022-07-15 19:16:54 -0300

    feat(FormGroup): add error status

--- fe/src/components/Input.js
+++ fe/src/components/Input.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.input`
   width: 100%;
@@ -21,4 +21,9 @@ export default styled.input`
     border: 2px solid ${({ theme }) => theme.colors.primary.main};
     transition: all .2s ease-in;
   }
+
+  ${({ theme, error }) => error && css`
+    color: ${theme.colors.danger.main};
+    border-color: ${theme.colors.danger.main} !important;
+  `}
 `;
