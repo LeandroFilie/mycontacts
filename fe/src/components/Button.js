55e62ee59a8f28178bfa708ae0ca419bb7ca32aa
Leandro Filie <leandro.gfilie@gmail.com>
2022-07-15 16:56:50 -0300

    feat(Button): add danger status

--- fe/src/components/Button.js
+++ fe/src/components/Button.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.button`
   height: 52px;
@@ -30,5 +30,17 @@ export default styled.button`
     background: #CCC;
     cursor: default;
   }
+
+  ${({ theme, danger }) => danger && css`
+    background: ${theme.colors.danger.main};
+
+    &:hover {
+      background: ${theme.colors.danger.light};
+    }
+
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
+  `}
 
 `;
